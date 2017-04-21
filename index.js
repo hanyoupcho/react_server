45651ad3772f6edec67b833e89bc61cbde2209c2
hanyoupcho <hanyoup.cho@gmail.com>
2017-04-21 17:24:16 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+const router = require('./router');
+
+// db
+mongoose.connect('mongodb://localhost:auth/auth');
+
+// configure
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+// server
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+
+server.listen(port);
+console.log('Server listning on:', port);
