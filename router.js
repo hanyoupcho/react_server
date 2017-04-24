c0be27dc934d765c5251c5f26a9d17c028734de1
hanyoupcho <hanyoup.cho@gmail.com>
2017-04-24 18:36:22 +0900

    Merge fix

--- router.js
+++ router.js
@@ -7,11 +7,7 @@ const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
   app.get('/', requireAuth, function(req, res) {
-<<<<<<< HEAD
     res.send( { message: 'super secret code is ABC123' });
-=======
-    res.send( { message: 'Supre secret code is ABC123'});
->>>>>>> origin/master
   })
   app.post('/signin', requireSignin, Authentication.signin);
   app.post('/signup', Authentication.signup);
